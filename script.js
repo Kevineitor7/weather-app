61e22aea13bcce0cde466192cdd15a62df4eb607
Kevin <qeven03@gmail.com>
2023-08-24 17:50:57 -0500

    weather app (in development)

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const time = document.querySelector('.time')
 const city = document.querySelector('.city')
 const country = document.querySelector('.country')
-const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temp')
+const feels = document.querySelector('.feels')
 const condition = document.querySelector('.condition')
-
-const date = new Date() 
-const hour = date.getHours() 
-const minute = String(date.getMinutes()).padStart(2, "0");
+const hoursTab = document.querySelector('.hours-tab')
+const hours = document.querySelector('.hours')
 
 function success(position) {
     const crd = position.coords;
     const latitude = crd.latitude
     const longitude = crd.longitude
   
-    console.log("Your current position is:");
     console.log(`Latitude : ${latitude}`);
     console.log(`Longitude: ${longitude}`);
 
@@ -28,16 +26,54 @@ function success(position) {
 
   async function fetching(lat,lon) {
     try {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=480bafb0e7094033b4b155606231408&q=${lat},${lon}`, {mode: 'cors'})
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=480bafb0e7094033b4b155606231408&q=${lat},${lon}&days=3`, {mode: 'cors'})
     const data = await response.json()
     console.log(data)
-    time.innerHTML += date.toLocaleDateString("en-US") + ' ' + hour + ':' + minute
+    time.innerHTML += data.location.localtime
     city.innerHTML += data.location.name + ', ' + data.location.region
     country.innerHTML += data.location.country
-    weather.innerHTML += Math.trunc(data.current.temp_c) + '&deg C' 
+    temperature.innerHTML += Math.trunc(data.current.temp_c) + '&degC' 
+    feels.innerHTML += 'Feels like ' + Math.trunc(data.current.feelslike_c) + '&degC'
     condition.innerHTML += data.current.condition.text
+    data.forecast.forecastday[0].hour.forEach( (element, i) => {
+      hours.innerHTML += `<div class="hours-info">
+  <div class="hour">${data.forecast.forecastday[0].hour[i].time.slice(10)}</div>
+  <div class="hour-icon"><img src="${data.forecast.forecastday[0].hour[i].condition.icon}" alt="no"></div>
+  <div class="hour-temp">${Math.trunc(data.forecast.forecastday[0].hour[i].temp_c)}&degC</div>
+  <div class="hour-humidity">Humidity: ${data.forecast.forecastday[0].hour[i].humidity}%</div>
+  <div class="hour-rain-snow">
+      <div class="hour-rain">
+          <div class="hour-rain-icon"><img src='https://www.iconarchive.com/download/i95046/custom-icon-design/lovely-weather-2/Humidity.ico' alt='no'></div>
+          <div class="hour-rain-chance">${data.forecast.forecastday[0].hour[i].chance_of_rain}%</div>
+      </div>
+      <div class="hour-snow">
+          <div class="hour-snow-icon"><img src='https://www.freeiconspng.com/thumbs/snow-icon/blue-snow-icon-8.png' alt='no'></div>
+          <div class="hour-snow-chance">${data.forecast.forecastday[0].hour[i].chance_of_snow}%</div>
+      </div>
+  </div>
+</div>`
+    })
     }
     catch(err) {
         console.log(err)
     }
 }
+
+/*function addHourInfo(data, index) {
+  hours.innerHTML += `<div class="hours-info">
+  <div class="hour">${data.forecast.forecastday[0].hour[index].time.slice(10)}</div>
+  <div class="hour-icon">sol</div>
+  <div class="hour-temp">41&degC</div>
+  <div class="hour-humidity">40%</div>
+  <div class="hour-rain-snow">
+      <div class="hour-rain">
+          <div class="hour-rain-icon">llu</div>
+          <div class="hour-rain-chance">0%</div>
+      </div>
+      <div class="hour-snow">
+          <div class="hour-snow-icon">sno</div>
+          <div class="hour-snow-chance">0%</div>
+      </div>
+  </div>
+</div>`
+}*/
